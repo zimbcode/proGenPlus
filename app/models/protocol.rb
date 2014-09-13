335425a966184ca260643a6297c0141678e7da25
Philipp Richter <p234850548@freenet.de>
2014-09-13 21:29:35 +0200

    tags added for topics

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -1,4 +1,5 @@
 class Protocol < ActiveRecord::Base
 	has_many :topics
 	accepts_nested_attributes_for :topics
+	has_many :tags, through: :topic
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,4 +2,6 @@ class Topic < ActiveRecord::Base
   belongs_to :protocol
   validates :headline, presence: true
   validates :text, presence: true
+  belongs_to :topic_tag
+  has_many :tags, through: :topic_tag
 end
