00586cd62641b43cb14d869ca78b463b90784cd6
Niklas A. Zbick <nicko.zbicko@gmail.com>
2014-09-14 10:46:06 +0200

    parser extended, list view added

--- app/models/protocol.rb
+++ app/models/protocol.rb
@@ -29,6 +29,7 @@ class Protocol < ActiveRecord::Base
 
 		pdf = Prawn::Document.new(page_size: 'A4', margin: 20.mm)
 		
+		pdf.font("Helvetica", :size => 10)
 		pdf.text "Protokoll #{dateCreated}"
 		pdf.move_down 20.mm
 
@@ -66,7 +67,22 @@ class Protocol < ActiveRecord::Base
 	 end
 
 	 def header(text, header_level)
-	   "pdf.text '#{text}', inline_format: :true\n"
+	 	size = "10.pt"
+	   case header_level
+	   when 1
+	   	size = "16.pt"
+	   when 2
+	   	size = "14.pt"
+	   when 3
+	   	size = "12.pt"
+	   when 5
+	   	size = "9.pt"
+	   when 6
+	   	size = "8.pt"	   		
+	   else
+	   	size = "10.pt"
+	   end
+	   "pdf.text '#{text}', inline_format: :true, size: " + size + ", style: :bold\n"
 	 end
 
 	 def paragraph(text)
@@ -94,11 +110,12 @@ class Protocol < ActiveRecord::Base
 	 end
 
 	 def list(text, something)
-	   "#{text}"
+	   "pdf.move_down 10.pt\n#{text}"
 	 end
 
 	 def list_item(text, list_level)
-	   "pdf.text '" + "- #{text}'\n".gsub("', inline_format: :true\n",'').gsub("pdf.text '",'').gsub("\n",'') + ", margin_left: 10.mm\n"
+	 	# FIXME: text_box mit Variabler Höhe beachten, derzeit nur "einzeilig", d.h. movedown um 20.pt pro Spiegelstrich
+	   "pdf.fill_circle  [7.mm, pdf.cursor - 4], 2\npdf.text_box '" + "#{text}'\n".gsub("', inline_format: :true\n",'').gsub("pdf.text '",'').gsub("\n",'') + ", at: [10.mm, pdf.cursor]\npdf.move_down 20.pt\n"
 	 end
 
 	end
